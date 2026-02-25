50eb46275cb3c67ca59e0bf2dea95a506fd67411
Katyusha47 <gamingiqbal703@gmail.com>
2026-02-25 09:00:07 +0700

    Add Vercel Analytics dependency and initialize it within the new App.vue component.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
 </template>
 
 <script setup>
+import { inject } from '@vercel/analytics'
+import { onMounted } from 'vue'
+
 defineOptions({
   name: 'App',
+})
+
+onMounted(() => {
+  inject() // Start Vercel Analytics
 })
 </script>
